61508d5a019096c0ad456b90d42238f0d584388b
SeAmen <1742914039@qq.com>
2024-01-26 00:17:19 +0800

    故事界面 背景模糊+文本框
    改了primary场景的相机和canvas，用来渲染个模糊图。改了node的位置position改为localposition,因为改了canvas的缩放

---
+++ Assets/Scripts/StoryText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+//using static System.Net.Mime.MediaTypeNames;
+
+public class StoryText : MonoBehaviour
+{
+    public Image bg;
+    public TextMeshProUGUI textMesh;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int tid;
+    string Str;
+    public void SetText(string text)
+    {
+        Str = text;
+
+        textMesh.text = Str;
+        bg.rectTransform.sizeDelta = textMesh.GetPreferredValues()+new Vector2(20f, 20f);
+
+        textMesh.text = "";
+        StartCoroutine(LoadText());
+
+    }
+    public IEnumerator LoadText()
+    { 
+        for(int i = 0; i < Str.Length; i++)
+        {
+            textMesh.text = Str.Substring(0,i);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
